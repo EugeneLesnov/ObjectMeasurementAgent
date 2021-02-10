099917a56c013588fba3d5cc0ac0b50a8be9a9fa
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-02-10 21:10:16 +0300

    feat: initial commit

---
+++ src/main/java/com/github/eugenelesnov/ObjectMeasurementAgent.java
@@ -0,0 +1,11 @@
+package com.github.eugenelesnov;
+
+/**
+ * @author Eugene Lesnov
+ */
+public class ObjectMeasurementAgent {
+
+    public static void premain(String args) {
+        System.out.println("Hello from agent!");
+    }
+}
